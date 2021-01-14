ce6b22b2da15060d90e8c65b5e5525ab67412a26
dnirns <daniel.irons@gmail.com>
2021-01-14 16:11:11 +0000

    dev branch first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container text-black text-4xl'>
+      <h1>Hello</h1>
+      <p className='text-9xl'>I AM ON THE DEV BRANCH</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
